f349949e4dd52987f69a1799de78d50af0768c13
Adrian Croitoru <adrian.croitoru.97@gmail.com>
2025-12-01 12:18:19 +0200

    hotfix

--- backend/src/main/java/com/example/notesApp/config/ApplicationConfig.java
+++ backend/src/main/java/com/example/notesApp/config/ApplicationConfig.java
@@ -2,7 +2,6 @@ package com.example.notesApp.config;
 
 import com.example.notesApp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
